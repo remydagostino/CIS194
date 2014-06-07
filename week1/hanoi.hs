760015489452ac8c2f04210a1faad9d975fbc3ba
Remy D'Agostino <dagostino.remy@gmail.com>
2014-06-07 23:09:45 +1000

    Completed week 1 hanoi problem

--- week1/hanoi.hs
+++ week1/hanoi.hs
@@ -3,23 +3,43 @@ module Hanoi where
 type Peg = String
 type Move = (Peg, Peg)
 
+-- Example 3 piece game
+-- 0. [1,2,3] [], []
+-- 1. [2,3] [1] []
+-- 2. [3] [1] [2]
+-- 3. [3] [] [1,2]
+-- 4. [] [3] [1,2]
+-- 5. [1] [3] [2]
+-- 6. [1] [2,3] []
+-- 7. [] [1,2,3] []
+
+-- Algorithm for 2 piece game
+-- hanoi 2 a b c
+-- (hanoi 1 a c b) + (a, b) + (hanoi 1 c b a)
+-- ((hanoi 0) + (a, c)) + (a, b) + ((hanoi 0) + (c, b) + (hanoi 0))
+-- (a, c) + (a, b) + (c, b)
+
 -- | Compute Towers of Hanoi
 --
+-- >>> hanoi 1 "a" "b" "c"
+-- [("a","b")]
+--
 -- >>> hanoi 2 "a" "b" "c"
--- [("a","c"), ("a","b"), ("c","b")]
+-- [("a","c"),("a","b"),("c","b")]
+--
+-- >>> hanoi 3 "a" "b" "c"
+-- [("a","b"),("a","c"),("b","c"),("a","b"),("c","a"),("c","b"),("a","b")]
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi 0 _ _ _ = []
-hanoi count p1 p2 p3 =
+hanoi 0 _ _ _  = []
+
+hanoi x start finish temp =
   let
-    state = ([1..count], [], [])
+    topToTemp = hanoi (x - 1) start temp finish
+    myMove = (start, finish)
+    tempToFinish = hanoi (x - 1) temp finish start
   in
-    -- Dummy
-    [(p1, p2)]
+    topToTemp ++ [myMove] ++ tempmpToFinish
 
 
--- define some function where i can store the state 
--- store state as [Int] [Int] [Int]
--- [1,2] [] []
--- [2] [1] []
--- [] [1] [2]
--- [] [] [1,2]
+
+
